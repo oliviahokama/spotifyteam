54602d52aff15eb4a46546593b356cab58f77953
Andrew Chan <atchan24@uw.edu>
2019-05-24 14:03:59 -0700

    Formatted song data

--- spotify_analysis.R
+++ spotify_analysis.R
@@ -1,6 +1,9 @@
 library("httr")
 library("dplyr")
 library("spotifyr")
+library(knitr)
+library(lubridate)
+library(ggplot2)
 
 songs <- read.csv("./data/chart2000-songmonth-0-3-0050.csv",
                   stringsAsFactors = F)
@@ -27,32 +30,10 @@ top_100_year <- top_songs_by_month %>%
   select(year, artist, song) %>%
   distinct()
 
-URI <- "https://api.spotify.com/v1"
-endpoint <- "/search"
-request <- paste0(URI, endpoint)
-params <- list("Authorization" = "d5c0d172605d428b9718156bd6fb164c",
-               "q" = "Despacito", "type" = "track")
-response <- GET(request, query = params)
-
 Sys.setenv(SPOTIFY_CLIENT_ID = 'd5c0d172605d428b9718156bd6fb164c')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '006a1517c2284a20bdc64547de50e799')
 
 access_token <- get_spotify_access_token()
-
-library(spotifyr)
-beatles <- get_artist_audio_features('the beatles')
-features <- search_spotify("Smooth", type = c("track"), limit = 1)
-
-library(knitr)
-library(lubridate)
-library(ggplot2)
-
-#song_ids <- c()
-
-#for (song in top_100_year$song) {
-#  id <- search_spotify(song, type = "track", limit = 1)
-#  song_ids <- append(song_ids, id$id)
-#}
 
 searchify <- function(song) {
   search_spotify(song, "track", limit = 1)
@@ -63,19 +44,35 @@ test <- lapply(top_100_year$song, searchify)
 top_100_year$data <- test
 
 top_100_year <- top_100_year %>% 
-  mutate(id = data$id)
-
-#get_my_top_artists_or_tracks(type = 'artists', time_range = 'short_term', limit = 5) %>% 
- # select(name, genres) %>% 
-  #rowwise %>% 
-  #mutate(genres = paste(genres, collapse = ', ')) %>% 
-  #ungroup %>% 
-  #kable()
-
-#my_id <- 'ohokama'
-#my_plists <- get_user_playlists(my_id)
+  filter(song != "Lady Marmalade (Voulez-Vous Coucher Aver Moi Ce Soir?)") %>% 
+  filter(song != "Moves Like Jagger")
 
 
-#my_plists2 <- my_plists %>%
- # filter(name %in% c('Taiwan Top 50', 'France Top 50', 'Bolivia Top 50', 'U.S. Top 50'))
+ids <- c()
+for (tibble in top_100_year$data) {
+  ids <- append(ids, tibble$id)
+}
 
+top_100_year$id <- ids
+
+song_info <- lapply(top_100_year$id, get_track_audio_features)
+
+danceability <- c()
+energy <- c()
+speechiness <- c()
+acousticness <- c()
+for (tibble in song_info) {
+  danceability <- append(danceability, tibble$danceability)
+  energy <- append(energy, tibble$energy)
+  speechiness <- append(speechiness, tibble$speechiness)
+  acousticness <- append(acousticness, tibble$acousticness)
+}
+
+top_100_year$danceability <- danceability
+top_100_year$energy <- energy
+top_100_year$speechiness <- speechiness
+top_100_year$acousticness <- acousticness
+
+# THIS IS THE DATASET WE WANT TO USE
+song_data <- top_100_year %>% 
+  select(year, song, artist, speechiness, energy, danceability, acousticness)
