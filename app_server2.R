7876970f129f9c390edd15dbae770dabfeb9e73e
Olivia Hokama <ohokama@gmail.com>
2019-06-05 02:58:09 -0700

    finalized scatterplot in version 2's

--- app_server2.R
+++ app_server2.R
@@ -5,9 +5,9 @@ library(stringr)
 
 top_2018 <- read.csv("data/top2018.csv", stringsAsFactors = F)
 
-shinyServer(function(input, output) { 
+server <- function(input, output) {
   output$scatter <- renderPlot({
     ggplot(data = top_2018) + geom_point(aes_string(x = "speechiness", y = input$chooseComparison), shape = strtoi(input$chooseShape)) + 
-      ggtitle(paste("ratings of attributes vs.", input$chooseComparison))
+      ggtitle(paste("speechiness vs.", input$chooseComparison))
   })
-})
+}
--- app_ui2.R
+++ app_ui2.R
@@ -3,36 +3,37 @@ library(shiny)
 library(lintr)
 library(plotly)
 
-# page_three <- tabPanel(
-#   titlePanel("Statistics for artists in 2018"),
-#   sidebarLayout(
-#     sidebarPanel(
-#       textInput("search", label = "Enter Artist", value = ""),
-#       br()
-#     ),
-#     mainPanel(
-#       plotlyOutput("scatter")))
-# )
-# 
-# ui <- navbarPage(
-#   "Artist comparisons in 2018",
-#   page_three
-# )
-
-
 page_artist <- tabPanel(
   
-  titlePanel("Artist Information"),
+  titlePanel("Speechiness comparisons"),
   sidebarLayout(
     sidebarPanel(
       
-      radioButtons("chooseComparison", "Choose what information you would like to see:",
-                   choices = list("Danceability" = "danceability", "Acousticness" = "acousticness", "Duration" = "duration_ms")),
+      radioButtons("chooseComparison", "Choose what to compare speechiness to based on the top songs in 2018:",
+                   choices = list("Danceability" = "danceability", 
+                                  "Acousticness" = "acousticness", 
+                                  "Duration" = "duration_ms",
+                                  "Energy" = "energy",
+                                  "Tempo" = "tempo")),
       
       selectInput("chooseShape", "Choose the shape of the data points:",
                   choices = list("Square" = "15", "Circle" = "16", "Triangle" = "17")) 
     ),
     
+    # p("This scatterplot visualizes how different attributes compare to speechiness
+    #   in the top songs of 2018. Using a dataset that holds the top 2018 songs, we 
+    #   wanted to look at how songs were related to speechiness to see if we saw a 
+    #   correlation between them. By clicking on the differnt options, try to see if 
+    #   you see any patterns between the two variables.",
+    # p("Danceability - How suitable the song is for dance"),
+    # p("Energy - The measure of intensity and musical activity"),
+    # p("Acousticness - The confidence that a track is acoustic"),
+    # p("Speechiness - The prominence of spoken words in a song"),
+    # p("Duration - The length of the song"),
+    # p("Tempo - The rhythm or beat measured of each song")
+    # 
+    # ),
+    # 
     mainPanel(
       plotOutput("scatter")
     )
@@ -40,7 +41,7 @@ page_artist <- tabPanel(
 )
 
 ui <- navbarPage(
-  "Artists in 2018",
+  "Songs in 2018: Speechiness Analysis",
   page_artist
 )
 
